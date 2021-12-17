906bd7b1c3bbab2eb3e5c97e88f6d047234ab40d
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-17 12:22:29 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BiologistComponent } from './pages/biologist/biologist.component';
+import { WebDeveloperComponent } from './pages/webDeveloper/web-developer.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"Biologist",
+  component: BiologistComponent
+},
+{
+  path:"Web-Developer",
+  component: WebDeveloperComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
