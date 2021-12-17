c30965f94268a9b21114eddbaf62fddaf4874d19
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-17 12:22:49 +0100

    Create biologist.component.ts

---
+++ src/app/pages/biologist/biologist.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: 'biologist.component.html'
+})
+
+export class BiologistComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
