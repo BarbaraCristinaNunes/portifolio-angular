8f06a400212de00b334af88023f5fb87658de298
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-02-10 16:54:07 +0100

    created variables to show title and about me

--- src/app/pages/biologist/biologist.component.ts
+++ src/app/pages/biologist/biologist.component.ts
@@ -1,18 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
-    templateUrl: 'biologist.component.html'
+    templateUrl: 'biologist.component.html',
+    styleUrls: ['biologist.component.css']
 })
 
 export class BiologistComponent implements OnInit {
     constructor(private httpclient: HttpClient) { }
 
+    myTitle = "Master in Ecology, Conservation and Wildlife Management";
+    aboutMe = "I am a biologist passionate for plant ecology, distribution patterns and conservations. My goal as a biologist is to work with plant conservation, mainly with endangered plants. My main skills as a biologist are plant identification, creating species distribution maps (ArcGIS), biological data management and project management.";
     datajason : any;
+    academicEducation : any;
+    publiccations : any;
 
     ngOnInit() { 
         this.httpclient.get('assets/workExperiences.json').subscribe((data:any) => {
             this.datajason = data?.workExperiences?.biologist;
+            this.academicEducation = data?.workExperiences?.webDeveloper.academicEducation;
+            this.publiccations = data?.workExperiences?.webDeveloper.publiccations;
         })
     }
 }
