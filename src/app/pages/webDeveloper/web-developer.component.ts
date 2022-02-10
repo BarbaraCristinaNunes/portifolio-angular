be2e16c08a4bf596366b908c8e6b3baf0efcfeec
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-02-10 16:53:32 +0100

    created variables to show title and about me

--- src/app/pages/webDeveloper/web-developer.component.ts
+++ src/app/pages/webDeveloper/web-developer.component.ts
@@ -2,16 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    templateUrl: 'web-developer.component.html'
+    templateUrl: 'web-developer.component.html',
+    styleUrls: ['web-developer.component.css']
 })
 
 export class WebDeveloperComponent implements OnInit {
     constructor(private httpclient: HttpClient) { }
 
+    myTitle = "Junior Full Stack Developer";
+    aboutMe = "I am a junior developer with a previous background in environmental areas as a biologist. My main interest now is to start an internship as a full stack where I can develop my skills in PHP, database, web design and can contribute with a team. I am very communicative, details and result oriented and a good time manager.";
     datajason : any;
+    academicEducation : any;
+
     ngOnInit() { 
         this.httpclient.get('assets/workExperiences.json').subscribe((data:any) => {
             this.datajason = data?.workExperiences?.webDeveloper;
+            this.academicEducation = data?.workExperiences?.webDeveloper.academicEducation;
         })
     }
 }
