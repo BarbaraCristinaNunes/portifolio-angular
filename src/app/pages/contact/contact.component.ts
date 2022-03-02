61fca4cc205ce0d1f8e8b03244cdfea1664a0c87
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-03-02 11:53:32 +0100

    Update contact.component.ts

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,15 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  templateUrl: 'contact.component.html',
+  styleUrls: ['contact.component.css']
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpclient: HttpClient) { }
 
+  datajason: any;
+  
   ngOnInit(): void {
+    this.httpclient.get('assets/workExperiences.json').subscribe((data:any) => {
+      this.datajason = data?.workExperiences?.contacts;
+  })
   }
 
 }
