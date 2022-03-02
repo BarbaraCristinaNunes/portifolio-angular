85735ad3a533e0a172b9bd14a20334f7bdaf5646
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-03-02 11:53:42 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BiologistComponent } from './pages/biologist/biologist.component';
 import { WebDeveloperComponent } from './pages/webDeveloper/web-developer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactComponent } from './pages/contact/contact.component';
 
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,11 +13,11 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { RouterModule } from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
