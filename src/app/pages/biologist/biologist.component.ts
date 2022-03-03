e8ec95c6037ca47987dc980003664a102f085d12
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-03-03 09:16:56 +0100

    Update biologist.component.ts

--- src/app/pages/biologist/biologist.component.ts
+++ src/app/pages/biologist/biologist.component.ts
@@ -10,20 +10,18 @@ export class BiologistComponent implements OnInit {
 
     myTitle = "Master in Ecology, Conservation and Wildlife Management";
     aboutMe = "I am a biologist passionate for plant ecology, distribution patterns and conservations. My goal as a biologist is to work with plant conservation, mainly with endangered plants. My main skills as a biologist are plant identification, creating species distribution maps (ArcGIS), biological data management and project management.";
-    datajason : any;
+
     academicEducation : any;
     publications : any;
     experiences : any;
     skills : any;
-    contacts : any;
+
     ngOnInit() { 
         this.httpclient.get('assets/workExperiences.json').subscribe((data:any) => {
-            this.datajason = data?.workExperiences?.biologist;
-            this.academicEducation = this.datajason.academicEducation;
-            this.publications = this.datajason.publications;
-            this.experiences = this.datajason.experiences;
-            this.skills = this.datajason.skills;
-            this.contacts = data?.workExperiences?.contacts;
+            this.academicEducation = data?.workExperiences?.biologist?.academicEducation;
+            this.publications = data?.workExperiences?.biologist?.publications;
+            this.experiences = data?.workExperiences?.biologist?.experiences;
+            this.skills = data?.workExperiences?.biologist?.skills;
         })
     }
 }
