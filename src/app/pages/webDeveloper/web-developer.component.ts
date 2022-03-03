3f18c9d71c628746da003dce4277a40089c9b030
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-03-03 09:16:51 +0100

    Update web-developer.component.ts

--- src/app/pages/webDeveloper/web-developer.component.ts
+++ src/app/pages/webDeveloper/web-developer.component.ts
@@ -11,7 +11,7 @@ export class WebDeveloperComponent implements OnInit {
 
     myTitle = "Junior Full Stack Developer";
     aboutMe = "I am a junior developer with a previous background in environmental areas as a biologist. My main interest now is to start an internship as a full stack where I can develop my skills in PHP, database, web design and can contribute with a team. I am very communicative, details and result oriented and a good time manager.";
-    datajason : any;
+    
     academicEducation : any;
     experiences : any;
     skills : any;
@@ -19,11 +19,10 @@ export class WebDeveloperComponent implements OnInit {
 
     ngOnInit() { 
         this.httpclient.get('assets/workExperiences.json').subscribe((data:any) => {
-            this.datajason = data?.workExperiences?.webDeveloper;
-            this.academicEducation = this.datajason.academicEducation;
-            this.experiences = this.datajason.experiences;
-            this.skills = this.datajason.skills;
-            this.projects = this.datajason.projects;
+            this.academicEducation = data?.workExperiences?.webDeveloper?.academicEducation;
+            this.experiences = data?.workExperiences?.webDeveloper?.experiences;
+            this.skills = data?.workExperiences?.webDeveloper?.skills;
+            this.projects = data?.workExperiences?.webDeveloper?.projects;
         })
     }
 }
